768bda18c302b86de70b716ee44bf11b0e62ebb0
José María Fernández <jose.m.fernandez@bsc.es>
2019-06-07 17:04:32 +0200

    Fixed unfinished metadata fixing logic

--- VRE_NF_RUNNER.py
+++ VRE_NF_RUNNER.py
@@ -98,16 +98,18 @@ def main_json(config, in_metadata, out_metadata):
     app = JSONApp()
     
     # Fixing possible problems in the input metadata
-    with open(in_metadata,"r") as in_met:
-        in_metaArr = json.load()
-        in_fixed = False
-        for in_m in in_metaArr:
-            if in_m.get('taxon_id',0)  == 0:
-                in_m['taxon_id'] = -1
+    with open(in_metadata,"r") as in_metF:
+        in_metaArr = json.load(in_metF)ç
+    
+    in_fixed = False
+    for in_m in in_metaArr:
+        if in_m.get('taxon_id',0)  == 0:
+            in_m['taxon_id'] = -1
+            in_fixed = True
     
     if in_fixed:
-        with open(in_metadata,"w") as in_met:
-            json.dump(in_metaArr,in_met)
+        with open(in_metadata,"w") as in_metF:
+            json.dump(in_metaArr,in_metF)
     
     result = app.launch(process_WF_RUNNER,
                         config,
