6f399286b69a0497973844356d786561bb13ef5b
José María Fernández <jose.m.fernandez@bsc.es>
2019-07-10 17:40:31 +0200

    Test script overhaul, so several tests can be run at once

--- test_VRE_NF_RUNNER.sh
+++ test_VRE_NF_RUNNER.sh
@@ -17,9 +17,32 @@ case "$BASEDIR" in
 		;;
 esac
 
-TOOL_EXECUTABLE="${BASEDIR}/VRE_NF_RUNNER"
-TEST_DATA_DIR="${BASEDIR}/tests/json/naive_workflow"
+eecho() {
+	echo "ERROR: $@" 1>&2
+}
 
-# The relative directory does matter!
-cd "$BASEDIR"
-time "$TOOL_EXECUTABLE" --config "$TEST_DATA_DIR"/config.json --in_metadata "$TEST_DATA_DIR"/in_metadata.json --out_metadata "$BASEDIR"/out_metadata.json --log_file "$BASEDIR"/tool.log
+TOOL_EXECUTABLE="${BASEDIR}/VRE_NF_RUNNER"
+TEST_BASE_DIR="${BASEDIR}/tests"
+if [ $# -gt  0 ] ; then
+	for TESTNAME in "$@" ; do
+		TEST_DATA_DIR="${TEST_BASE_DIR}/$TESTNAME"
+		if [ -d "$TEST_DATA_DIR" ] ; then
+			TEST_WORK_DIR="${BASEDIR}/${TESTNAME}-test"
+			rm -rf "$TEST_WORK_DIR"
+			mkdir -p  "$TEST_WORK_DIR"
+			# The relative directory does matter!
+			cd "$BASEDIR"
+			echo
+			echo "INFO: Running test $TESTNAME"
+			time "$TOOL_EXECUTABLE" --config "$TEST_DATA_DIR"/config.json --in_metadata "$TEST_DATA_DIR"/in_metadata.json --out_metadata "${TEST_WORK_DIR}/${TESTNAME}-out_metadata.json" --log_file "${TEST_WORK_DIR}/${TESTNAME}-test.log"
+			echo
+			echo "INFO: Test $TESTNAME return value $?"
+		else
+			eecho "Test set identifier $TESTNAME is invalid. Skipping..."
+		fi
+	done
+else
+	eecho "You must pass at least a test set identifier (i.e. the name of a directory in $TEST_BASE_DIR)" 
+fi
+
+
